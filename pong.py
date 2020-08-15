c77b28b1741ecb46d18bb5a1f9a3f119487464ab
sthasadin <shrestha.sadin007@gmail.com>
2020-08-15 14:54:15 +0545

    added paddle b

--- pong.py
+++ pong.py
@@ -8,7 +8,6 @@ wn.tracer(0)  #stop the screen from refreshing and will help to speed up the gam
 
 
 #Paddle A
-
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -16,7 +15,17 @@ paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
+
+
 #Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
 
 #Ball
 
