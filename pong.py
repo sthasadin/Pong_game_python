08031bc0b6aca75c9b00762050e7a92dd048f584
sthasadin <shrestha.sadin007@gmail.com>
2020-08-15 15:13:09 +0545

    added paddle_b movement

--- pong.py
+++ pong.py
@@ -46,10 +46,24 @@ def paddle_a_down():
   y -=20
   paddle_a.sety(y)
 
+
+def paddle_b_up():
+  y = paddle_b.ycor() 
+  y +=20
+  paddle_b.sety(y)
+
+def paddle_b_down():
+  y = paddle_b.ycor() 
+  y -=20
+  paddle_b.sety(y)
+
 #Keyboard Binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
+
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 
 #main game loop 
