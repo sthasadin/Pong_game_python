387ebd243954da345eb9004e31bc7f81efbeca07
sthasadin <shrestha.sadin007@gmail.com>
2020-08-16 15:08:00 +0545

    added sound to the strikes

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
 import turtle
+import winsound
 
 wn=turtle.Screen()
 wn.title("Pong by @stha_sadin")
@@ -95,10 +96,12 @@ while True:
   if ball.ycor() > 290:
     ball.sety(290)
     ball.dy *= -1
+    winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
   if ball.ycor() < -290:
     ball.sety(-290)
     ball.dy *= -1
+    winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
 
   if ball.xcor() >390:
@@ -122,8 +125,10 @@ while True:
   if (ball.xcor()> 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
     ball.setx(340)
     ball.dx *= -1
+    winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
 
   if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
     ball.setx(-340)
     ball.dx *= -1
+    winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
